ab3b6d7dc00c86aebc125eee73cd8b1d09735dd7
itamm15 <osinski.mateusz15@gmail.com>
2025-11-11 21:23:42 +0100

    chore(ex_aws): configure ExAws

--- config/config.exs
+++ config/config.exs
@@ -60,6 +60,17 @@ config :logger, :default_formatter,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configure ExAws
+config :ex_aws,
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
+  region: "us-east-1"
+
+config :ex_aws, :kms,
+  scheme: "http://",
+  host: "localhost",
+  port: 4566
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
